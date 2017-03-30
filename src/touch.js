c9ba7e163240ed86acad33018b364eb9eaa005d7
answershuto <905307789@qq.com>
2017-03-30 22:35:58 +0800

    增加手势监听事件

--- src/touch.js
+++ src/touch.js
@@ -2,7 +2,19 @@
 	win.touch = factory();
 })(window, document, function(){
 	function toucher(dom) {
-		
+		this.dom = dom;
+		dom.addEventListener('touchstart', function(){
+			console.log('touchstart')
+		});
+		dom.addEventListener('touchend', function(){
+			console.log('touchend')
+		});
+		dom.addEventListener('touchmove', function(){
+			console.log('touchmove')
+		});
+		dom.addEventListener('touchcancel', function(){
+			console.log('touchcancel')
+		});
 	}
 
 	return toucher;
