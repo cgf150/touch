bd27ba467f66c615d0adf8fce94265c1a5095d51
answershuto <905307789@qq.com>
2017-04-13 17:00:59 +0800

    增加attach与detech方法注册以及删除对应事件的回调函数

--- src/touch.js
+++ src/touch.js
@@ -1,21 +1,64 @@
 (function(win, doc, factory) {
 	win.touch = factory();
 })(window, document, function(){
-	function toucher(dom) {
-		this.dom = dom;
-		dom.addEventListener('touchstart', function(){
-			console.log('touchstart')
-		});
-		dom.addEventListener('touchend', function(){
-			console.log('touchend')
-		});
-		dom.addEventListener('touchmove', function(){
-			console.log('touchmove')
-		});
-		dom.addEventListener('touchcancel', function(){
-			console.log('touchcancel')
-		});
+	var that = this;
+
+	function start(e) {
+		
 	}
 
-	return toucher;
+	function end(e) {
+
+	}
+
+	function move(e) {
+
+	}
+
+	function cancel(e) {
+
+	}
+
+	function toucher(dom) {
+		this.dom = dom;
+		/*事件回调对象*/
+		this.events = {};
+
+		dom.addEventListener('touchstart', start);
+		dom.addEventListener('touchend', end);
+		dom.addEventListener('touchmove', move);
+		dom.addEventListener('touchcancel', cancel);
+	}
+
+	toucher.prototype.attach = function(eventName, callBack) {
+		if ('string' !== typeof(eventName) || 'function' !== typeof(callBack)) return this;
+
+		!this.events[eventName] && (this.events[eventName] = []);
+
+		this.events[eventName].push({
+			callBack: callBack
+		})
+		
+		return this;
+	}
+
+	toucher.prototype.detech = function(eventName, callBack) {
+		if ('string' !== typeof(eventName) || 'function' !== typeof(callBack)) return this;
+
+		for(i in this.events) {
+			if (i === eventName) {
+				this.events[i].forEach(function(item, index) {
+					if (callBack === item.callBack) {
+						this.events[i].splice(index, 1);
+					}
+				}.bind(this))
+			}
+		}
+		
+		return this;
+	}
+
+	return function(dom) {
+		return new toucher(dom);
+	};
 });
