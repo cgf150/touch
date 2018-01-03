a540b93cd7d5738501f2a48d038ca7220e63ec42
answershuto <905307789@qq.com>
2018-01-03 20:06:26 +0800

    chore:restore.

--- vueSrc/store/actions.js
+++
@@ -1,5 +0,0 @@
-'use strict';
-
-let actions = {};
-
-module.exports = actions;
--- vueSrc/store/state.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-module.exports = {
-	Route:'/',
-	isLoading: false,
-	isAlert: false,
-	AlertMessage: "",
-	Page: "Home"
-}
